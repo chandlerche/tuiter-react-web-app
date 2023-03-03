553fb4c1efa65ebc306b909e1e475f348c38c3d4
“chandlerche” <54366747+chandlerche@users.noreply.github.com>
2023-03-03 01:05:01 -0500

    a6

--- src/App.js
+++ src/App.js
@@ -11,12 +11,9 @@ function App() {
       <BrowserRouter>
           <div className="container">
               <Routes>
-                  <Route index
-                         element={<Labs/>}/>
-                  <Route path="/hello"
-                         element={<HelloWorld/>}/>
-                  <Route path="/tuiter"
-                         element={<Tuiter/>}/>
+                  <Route index element={<Labs/>}/>
+                  <Route path="/hello" element={<HelloWorld/>}/>
+                  <Route path="/tuiter/*" element={<Tuiter/>}/>
               </Routes>
           </div>
       </BrowserRouter>
