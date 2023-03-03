643c5dafc3089655ddceb6048e1e6d1b2a896322
“chandlerche” <54366747+chandlerche@users.noreply.github.com>
2023-03-02 23:16:01 -0500

    a6

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,14 +1,14 @@
 import {Link}
     from "react-router-dom";
 
-function HelloWorld() {
+function Tuiter() {
     return(
         <div>
             <Link to="/">Lab</Link> |
             <Link to="/hello">Hello</Link> |
             <Link to="/tuiter">Tuiter</Link>
-            <h1>Hello World!</h1>
+            <h1>Tuiter</h1>
         </div>
     )
-};
-export default HelloWorld;
+}
+export default Tuiter
