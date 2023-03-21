02280a8e14c13b364f1c1b2e8c873ab72a91251c
“chandlerche” <54366747+chandlerche@users.noreply.github.com>
2023-03-21 16:39:27 -0400

    a7

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -5,9 +5,18 @@ import WhoToFollowList from "./who-to-follow-list";
 import {Routes, Route} from "react-router";
 import HomeComponent from "./home";
 import PostSummaryList from "./post-summary-list";
+import whoReducer
+    from "./reducers/who-reducer";
+import tuitsReducer from "./tuits/tuits-reducer";
+import { configureStore }
+    from '@reduxjs/toolkit';
+import {Provider} from "react-redux";
+const store = configureStore(
+    {reducer: {who: whoReducer, tuits: tuitsReducer}});
 
 function Tuiter() {
     return (
+        <Provider store={store}>
         <div className="row mt-2">
             <div className="col-2 col-md-2 col-lg-1 col-xl-2">
                 <NavigationSidebar active="explore"/>
@@ -26,6 +35,7 @@ function Tuiter() {
                 </Routes>
             </div>
         </div>
+        </Provider>
     );
 }
 
